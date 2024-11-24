6ae167de38e86402397d485fdfa3d31d36d1026c
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2024-11-23 23:52:18 -0300

    Run pre-commit checks on all files

--- src/pipelines_from_scratch/components.py
+++ src/pipelines_from_scratch/components.py
@@ -1,6 +1,9 @@
 import kfp.dsl
 
 
-@kfp.dsl.component(base_image="python:3.10", target_image="us-central1-docker.pkg.dev/pipelines-from-scratch/components/add:0.1.0")
+@kfp.dsl.component(
+    base_image="python:3.10",
+    target_image="us-central1-docker.pkg.dev/pipelines-from-scratch/components/add:0.1.0",
+)
 def add(a: int, b: int) -> int:
     return a + b
