a8224a506667e7208f75eb79c993e665081e7ba9
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2024-11-26 19:54:49 -0300

    feat: refactor components to use package version

--- src/pipelines_from_scratch/pipelines.py
+++ src/pipelines_from_scratch/pipelines.py
@@ -4,7 +4,7 @@ import pipelines_from_scratch.components as components
 
 
 @kfp.dsl.pipeline
-def math_pipeline(a: int, b: int, c: int) -> str:
+def math_pipeline(a: int, b: int, c: int) -> int:
     t1 = components.add(a=a, b=b)
     t2 = components.add(a=t1.output, b=c)
     return t2.output
