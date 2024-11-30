05d8b43b9ebe4cbc7658246430d0f5ec5e802ebc
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2024-11-29 21:59:53 -0300

    refactor: move predefined component argument to partial object

--- src/pipelines_from_scratch/components.py
+++ src/pipelines_from_scratch/components.py
@@ -1,3 +1,5 @@
+import functools
+
 import kfp.dsl
 
 import pipelines_from_scratch
@@ -5,6 +7,7 @@ import pipelines_from_scratch
 
 DOCKER_REGISTRY = "us-central1-docker.pkg.dev/pipelines-from-scratch"
 COMPONENTS_REPO = "components"
+IMAGE_NAME = f"{DOCKER_REGISTRY}/{COMPONENTS_REPO}/{pipelines_from_scratch.__name__}"
 # NOTE: Python version uses "+" (plus sign) to separate version from local
 # specifier, e.g. 1.2.1.dev2+g42eecf3.d20241126224450. The plus sign is an
 # invalid character in a Docker tag, so we replace it for a valid character.
@@ -14,9 +17,14 @@ COMPONENTS_REPO = "components"
 DOCKER_TAG = pipelines_from_scratch.__version__.replace("+", "-")
 
 
-@kfp.dsl.component(
-    base_image="python:3.10-slim",
-    target_image=f"{DOCKER_REGISTRY}/{COMPONENTS_REPO}/add:{DOCKER_TAG}",
+# NOTE: on the lack of base_image argument, we handle the image ourselves via
+# the Dockerfile found in the project root.
+component = functools.partial(
+    kfp.dsl.component,
+    target_image=f"{IMAGE_NAME}:{DOCKER_TAG}",
 )
+
+
+@component
 def add(a: int, b: int) -> int:
     return a + b
